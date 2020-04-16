483bb0ca515092e64c2eec3d6de3fbf538d66544
Velaska <lerikald@gmail.com>
2020-04-16 08:47:02 +0300

    add navigation on site

--- js/scripts.js
+++ js/scripts.js
@@ -41,6 +41,12 @@ $(document).ready(function(){
             return false;
         });
     });
+    $(".navbar-nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
 
 
